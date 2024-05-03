3da5991ff5b967326b6faf228340bfaf81291e4c
48lexR <cbull358@gmail.com>
2024-05-02 21:36:24 -0400

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,11 @@ export default function RootLayout({
         name="google-adsense-account"
         content="ca-pub-7368532165347168"
       ></meta>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7368532165347168"
+        crossOrigin="anonymous"
+      ></script>
       <body className={inter.className}>
         <NavBar></NavBar>
         {children}
